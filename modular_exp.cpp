25f76631d8b0946ed50fdf853a45aae0c1ba76aa
Sahak Petrosyan <73205649+SahakPetrosyan638@users.noreply.github.com>
2022-04-01 16:59:31 -0400

    Add files via upload

--- modular_exp.cpp
+++ modular_exp.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
-    int p {1113};
-    int g {5633};
-    int e {933};
-    int v {g%p};
+    int p {0};
+    int g {0};
+    int e {0};
+
+    std::cout << "Enter the modulo p:" << std::endl;
+    std::cin >> p;
+    
+    std::cout << "Enter the base g:" << std::endl;
+    std::cin >> g;
+    
+    std::cout << "Enter the exponent e:" << std::endl;
+    std::cin >> e;
+    
+    int e1 {e};
+    int v {g % p};
     int t {1};
     
     while(e > 0)
@@ -15,11 +24,12 @@ int main()
         if(e % 2 == 1)
             t = (t*v)%p;
     
-        e = (e/2);
-        v = (v*v)%p;
+        e /= 2;
+        v = (v*v) % p;
     }
     
-    cout << t << "is equal (563**93 mod 113)" <<endl;
+    std::cout << "(" << g << " ^ " << e1 << ") mod " << p << " = " << t << std::endl;
     
     return 0;
 }
+
