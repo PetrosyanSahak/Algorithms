e6976023b00ba8a34ab9766eea79ebbb70761889
Sahak Petrosyan <73205649+SahakPetrosyan638@users.noreply.github.com>
2022-04-01 17:25:15 -0400

    Update euclid_gcd.cpp

--- euclid_gcd.cpp
+++ euclid_gcd.cpp
@@ -1,17 +1,16 @@
 #include<iostream>
 
-using namespace std;
-
 int gcd(int, int);
 
 int main()
 {
 	int a {0};
 	int b {0};
-	cin >> a >> b;
-	cout << gcd(a,b);
-
-
+	
+	std::cout << "Enter 2 number` a and b, to get the gcd(a,b): " << std::endl;
+	std::cin >> a >> b;
+	std::cout << gcd(a,b);
+	
 	return 0;
 }
 
