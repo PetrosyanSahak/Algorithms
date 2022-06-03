5f84b2f798b171e500588d47f7b8e6a305deaf27
Petrosyan Sahak <73205649+PetrosyanSahak@users.noreply.github.com>
2022-06-03 11:55:11 -0400

    Update prime_generator_finder.cpp

--- prime_generator_finder.cpp
+++ prime_generator_finder.cpp
@@ -1,37 +1,48 @@
+//In short, a number {k} between (1,prime) will be generator,
+//if there is no number {d} in range(1,prime-1), that will satisfy this equation
+//							k**d == 1 mod (prime)
 #include<iostream>
 #include<cmath>
 
 int main()
 {
 
-    int p {0};
-    int g {0};
-    int e {0};
-    int k {0};
+    int prime {0};
+    int generator {0};
+    int exponent {0};
+    int generator_count {0};
     
     std::cout << "Enter the prime p: " << std::endl;
-    std::cin >> p;
-    std::cout << "\nHere are the generators for prime: " << p << std::endl;
+    std::cin >> prime;
+    std::cout << "\nHere are the generators for prime: " << prime << std::endl;
     std::cout << "--------------------------------------\n" << std::endl;
 
-    for(g = 2; g < (p-1); g++)
+    //This loop iterates over numbers [2,prime-2], to test if they are generator for a given prime
+    //Note that we do not test (prime-1) because Fermat's Law states that
+    //		                                                          a**(prime-1) = 1 (mod prime)
+    //                                                        for any natural number a
+    for(generator = 2; generator < (prime-1); generator++)
     {
-        e = 2;
+	//Maybe this assignment doesnot change anything, I am not sure!?
+        exponent = 2;
         
-	for(e = 2; e < (p-1); e++)
+	for(exponent = 2; exponent < (prime - 1); exponent++)
         {
-            int f = pow(g,e);
-
-            if(f%p == 1)
+            int intermediate_value = pow(generator,exponent);
+            //
+	    //if the residue is 1, means we have cycle, but because generator is less than (prime-1)
+	    //this number cannot be generator, thus we break from this loop, and test the remaining values
+            if(intermadiate_value % prime == 1)
                 break;
         }
         
-        if(e == (p-1))
+	//if we did not break out from the previous loop, it means that we found a generator
+        if(exponent == (prime - 1))
 	{
-		std::cout << ++k << ": -> " << g << std::endl;
+		std::cout << ++generator_count << ": -> " << generator << std::endl;
 	}
     }
-    std::cout << "\nFound " << k << " generators" << std::endl;
+    std::cout << "\nFound " << generator_count << " generators" << std::endl;
 	
     return 0;
 }
