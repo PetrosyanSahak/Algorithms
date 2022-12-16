f12714272f8e5320b5cfaf5a02f6c8c4a0df2d59
Petrosyan Sahak <73205649+PetrosyanSahak@users.noreply.github.com>
2022-12-16 15:58:44 +0400

    Update prime_generator_finder.cpp
    removed comments

--- prime_generator_finder.cpp
+++ prime_generator_finder.cpp
@@ -1,6 +1,3 @@
-//In short, a number {k} between (1,prime) will be generator,
-//if there is no number {d} in range(1,prime-1), that will satisfy this equation
-//							k**d == 1 mod (prime)
 #include<iostream>
 #include<cmath>
 
@@ -15,24 +12,16 @@ int main()
     std::cout << "\nHere are the generators for prime: " << prime << std::endl;
     std::cout << "--------------------------------------\n" << std::endl;
 
-    //This loop iterates over numbers [2,prime-2], to test if they are generator for a given prime
-    //Note that we do not test (prime-1) because Fermat's Law states that
-    //		                                                          a**(prime-1) = 1 (mod prime)
-    //                                                        for any natural number a
     for(int generator {2}; generator < (prime-1); ++generator)
     {
         int exponent;
 	    
 	for(exponent = 2; exponent < (prime - 1); ++exponent)
-            //
-	    //if the residue is 1, means we have cycle, but because generator is less than (prime-1)
-	    //this number cannot be generator, thus we break from this loop, and test the remaining values
             if( (int)pow(generator,exponent) % prime == 1)
                 break;
         
-	//if we did not break out from the previous loop, it means that we found a generator
-        if(exponent == (prime - 1))
-		std::cout << ++generator_count << ": -> " << generator << std::endl;
+	if(exponent == (prime - 1))
+            std::cout << ++generator_count << ": -> " << generator << std::endl;
     }
     std::cout << "\nFound " << generator_count << " generators" << std::endl;
 	
