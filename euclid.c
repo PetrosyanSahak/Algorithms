c4570599ef3ebf3a66f36f0ce426870d0e6389ea
Petrosyan Sahak <73205649+PetrosyanSahak@users.noreply.github.com>
2022-12-22 19:33:38 +0400

    Create euclid.c

---
+++ euclid.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int gcd(int, int);
+
+int main() {
+	int a = 0;
+	int b = 0;
+	
+	printf("%s", "Enter 2 number` a and b, to get the gcd(a,b): \n");
+  scanf("%d%d", &a, &b);
+	printf("%d" gcd(a,b));
+	
+	return 0;
+}
+
+int gcd(int a, int b) {
+	int r = a % b;
+		
+	while(r != 0) {
+		a = b;
+		b = r;
+		r = a % b;
+	}
+  
+	return b;
+}
